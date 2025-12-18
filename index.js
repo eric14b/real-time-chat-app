c058b6eca32274806075719f394a53b372f530f1
Eric Batsaikhan <eric.batsaikhan2@gmail.com>
2025-12-18 14:13:49 -0500

    Finished routin tutorial, now working on user auth

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.post('/echo', (req, res) => {
+  res.json({
+    received: req.body
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
